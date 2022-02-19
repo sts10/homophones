2f8dfcc3b83fbd76d6495441364585c87b72bb0c
Sam Schlinkert <sschlinkert@gmail.com>
2022-02-18 22:23:52 -0500

    implements clap

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,87 @@
+use clap::Parser;
 use scraper::{Html, Selector};
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Write;
+use std::path::Path;
 use std::path::PathBuf;
 use std::{thread, time};
 
-fn main() {
-    let words = [
-        "sun".to_string(),
-        "there".to_string(),
-        "marvelous".to_string(),
-    ];
-    // let input = PathBuf::from("input_list.txt");
-    // let words = make_vec_from_filenames(&[input]);
+/// Generate lists of homophones
+#[derive(Parser, Debug)]
+#[clap(version, about, name = "homophones")]
+struct Args {
+    /// Path for outputted file for list of PAIRS of homohpones.
+    #[clap(short = 'p', long = "pairs", parse(from_os_str))]
+    pairs_output: Option<PathBuf>,
 
+    /// Path for outputted file for list of SINGLE homohpones.
+    #[clap(short = 's', long = "singles", parse(from_os_str))]
+    singles_output: Option<PathBuf>,
+
+    /// Force overwrite of output file if it exists.
+    #[clap(short = 'f', long = "force")]
+    force_overwrite: bool,
+
+    /// Word list input files. Can be more than one.
+    #[clap(name = "Inputted Word Lists", parse(from_os_str), required = true)]
+    inputted_word_lists: Vec<PathBuf>,
+}
+
+fn main() {
+    let args = Args::parse();
+    if !validate(&args) {
+        return;
+    }
+
+    let words = make_vec_from_filenames(&args.inputted_word_lists);
+
+    // This make_pairs_of_homophones does the actual web-scraping
+    // so we only want to run it once
     let pairs_of_homophones = make_pairs_of_homophones(&words);
-    write_tuples_to_file(&pairs_of_homophones);
-    write_vec_to_file(singularize(&pairs_of_homophones));
+    if let Some(ref pairs_output) = args.pairs_output {
+        write_tuples_to_file(&pairs_of_homophones, pairs_output);
+    }
+    if let Some(ref singles_output) = args.singles_output {
+        write_vec_to_file(singularize(&pairs_of_homophones), singles_output);
+    }
+}
+
+fn validate(args: &Args) -> bool {
+    match (&args.pairs_output, &args.singles_output) {
+        (Some(pairs_output), Some(singles_output)) => {
+            if !args.force_overwrite
+                && (Path::new(&pairs_output).exists() || Path::new(&singles_output).exists())
+            {
+                eprintln!(
+                    "One of the specified output files already exists. Use --force flag to force an overwrite."
+                );
+                return false;
+            }
+        }
+        (Some(pairs_output), None) => {
+            if !args.force_overwrite && Path::new(&pairs_output).exists() {
+                eprintln!(
+                    "Specified output file already exists. Use --force flag to force an overwrite."
+                );
+                return false;
+            }
+        }
+        (None, Some(singles_output)) => {
+            if !args.force_overwrite && Path::new(&singles_output).exists() {
+                eprintln!(
+                    "Specified output file already exists. Use --force flag to force an overwrite."
+                );
+                return false;
+            }
+        }
+        (None, None) => {
+            eprintln!("Error: Must specify an output file location for either a list of homophone pairs (-p) or list of single homophones (-s)");
+            return false;
+        }
+    }
+    true
 }
 
 fn singularize(pairs_of_homophones: &[(String, String)]) -> Vec<String> {
@@ -33,7 +97,6 @@ fn make_pairs_of_homophones(input_words: &[String]) -> Vec<(String, String)> {
     let mut tuples_of_homophones = vec![];
     for word in input_words {
         if let Some(list_of_homophones) = get_homophones(word) {
-            println!("{} {:?}", word, list_of_homophones);
             for homophone in list_of_homophones {
                 tuples_of_homophones.push((word.clone(), homophone));
             }
@@ -102,7 +165,8 @@ pub fn make_vec_from_filenames(filenames: &[PathBuf]) -> Vec<String> {
             word_list.push(l);
         }
     }
-    word_list
+    // word_list
+    sort_and_dedup(&mut word_list)
 }
 
 /// Alphabetizes and de-duplicates a Vector of `String`s.
@@ -116,17 +180,16 @@ fn sort_and_dedup(list: &mut Vec<String>) -> Vec<String> {
     list.to_vec()
 }
 
-fn write_tuples_to_file(vec: &[(String, String)]) {
-    let output = "homophone_pairs.txt";
+fn write_tuples_to_file(vec: &[(String, String)], output: &Path) {
+    // let output = "homophone_pairs.txt";
     let mut f = File::create(output).expect("Unable to create file");
     for tuple in vec {
-        println!("Writing {} and {} to file", tuple.0, tuple.1);
         writeln!(f, "{},{}", tuple.0, tuple.1).expect("Unable to write word to file");
     }
 }
 
-fn write_vec_to_file(vec: Vec<String>) {
-    let output = "homophones.txt";
+fn write_vec_to_file(vec: Vec<String>, output: &Path) {
+    // let output = "homophones.txt";
     let mut f = File::create(output).expect("Unable to create file");
     for string in vec {
         writeln!(f, "{}", string).expect("Unable to write word to file");
